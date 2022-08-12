d259d7d1e4ebbe87eba2840936edba26a378ad0e
Joachim Wright <joewright1610@gmail.com>
2022-08-12 15:10:30 -0400

    Remove a bit of boilerplate.

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,32 @@ use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
 use winit_input_helper::WinitInputHelper;
 
-const WIDTH: u32 = 320;
-const HEIGHT: u32 = 240;
-const BOX_SIZE: i16 = 64;
+use std::collections::HashMap;
+use std::env;
+use std::fs;
 
-/// Representation of the application state. In this example, a box will bounce around the screen.
-struct World {
-    box_x: i16,
-    box_y: i16,
-    velocity_x: i16,
-    velocity_y: i16,
+const WIDTH: usize = 320;
+const HEIGHT: usize = 240;
+
+struct Chip8 {
+    memory: Vec<u8>,
+    register: HashMap<u8, u8>,
+    I: u16,
+    sound_timer: u8,
+    delay_timer: u8,
+    pc: u16,
+    sp: u8,
+    stack: Vec<u8>,
+    keys: [bool; 12],
+    screen: [[u8; HEIGHT]; WIDTH]
 }
 
 fn main() -> Result<(), Error> {
+
+    let file = std::env::args().nth(1).expect("please supply a file");
+    let mut chip8 = Chip8::new();
+    chip8.memory = fs::read(file).unwrap();
+
     env_logger::init();
     let event_loop = EventLoop::new();
     let mut input = WinitInputHelper::new();
@@ -38,14 +51,13 @@ fn main() -> Result<(), Error> {
     let mut pixels = {
         let window_size = window.inner_size();
         let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+        Pixels::new(WIDTH as u32, HEIGHT as u32, surface_texture)?
     };
-    let mut world = World::new();
 
     event_loop.run(move |event, _, control_flow| {
         // Draw the current frame
         if let Event::RedrawRequested(_) = event {
-            world.draw(pixels.get_frame());
+            chip8.draw(pixels.get_frame());
             if pixels
                 .render()
                 .map_err(|e| error!("pixels.render() failed: {}", e))
@@ -70,34 +82,32 @@ fn main() -> Result<(), Error> {
             }
 
             // Update internal state and request a redraw
-            world.update();
+            chip8.update();
             window.request_redraw();
         }
     });
 }
 
-impl World {
+impl Chip8 {
     /// Create a new `World` instance that can draw a moving box.
     fn new() -> Self {
         Self {
-            box_x: 24,
-            box_y: 16,
-            velocity_x: 1,
-            velocity_y: 1,
+            memory: Vec::new(),
+            register: HashMap::new(),
+            I: 0,
+            sound_timer: 0,
+            delay_timer: 0,
+            pc: 0x200,
+            sp: 0,
+            stack: Vec::new(),
+            keys: [false; 12],
+            screen: [[0; HEIGHT]; WIDTH]
         }
     }
 
     /// Update the `World` internal state; bounce the box around the screen.
     fn update(&mut self) {
-        if self.box_x <= 0 || self.box_x + BOX_SIZE > WIDTH as i16 {
-            self.velocity_x *= -1;
-        }
-        if self.box_y <= 0 || self.box_y + BOX_SIZE > HEIGHT as i16 {
-            self.velocity_y *= -1;
-        }
-
-        self.box_x += self.velocity_x;
-        self.box_y += self.velocity_y;
+        
     }
 
     /// Draw the `World` state to the frame buffer.
@@ -105,19 +115,10 @@ impl World {
     /// Assumes the default texture format: `wgpu::TextureFormat::Rgba8UnormSrgb`
     fn draw(&self, frame: &mut [u8]) {
         for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
-            let x = (i % WIDTH as usize) as i16;
-            let y = (i / WIDTH as usize) as i16;
+            let _x = (i % WIDTH as usize) as i16;
+            let _y = (i / WIDTH as usize) as i16;
 
-            let inside_the_box = x >= self.box_x
-                && x < self.box_x + BOX_SIZE
-                && y >= self.box_y
-                && y < self.box_y + BOX_SIZE;
-
-            let rgba = if inside_the_box {
-                [0x5e, 0x48, 0xe8, 0xff]
-            } else {
-                [0x48, 0xb2, 0xe8, 0xff]
-            };
+            let rgba = [0x5e, 0x48, 0xe8, 0xff];
 
             pixel.copy_from_slice(&rgba);
         }
